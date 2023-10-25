86cf8173b977b50eb3d1b29346ad509c5bdc0d00
andrejapetrovic <33402257+andrejapetrovic@users.noreply.github.com>
2023-10-25 10:38:21 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 app.use(express.static('static'))
+app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-app.get('/', (req, res) => {
-
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'about.html'));
 })
-*/
+
+app.get('/services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'services.html'));
+})
+
+app.get('/news', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'news.html'));
+})
+
+app.get('/references', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'references.html'));
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'contact.html'));
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}!`))
